df08ef8670931dd0b8fc0768c8958cae8b0cbd1d
Isaac <isaacgoss14750@gmail.com>
2018-04-10 20:13:06 -0400

    added code for labeling dataset

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 """How to train your neural network."""
 
+import numpy
 import cleansing
 from PIL import Image, ImageDraw
 from network import Network
@@ -10,7 +11,7 @@ def lowres(pics):
     Takes a list of images and reduces both dimensions by a quarter."""
     res = []
     for pic in pics:
-        width, height = pic.size()
+        width, height = pic.size
         res.append(pic.resize((width//4, height//4)))
     return res
 
@@ -34,20 +35,40 @@ def draw_grid(image):
         line = ((x_start, y), (x_end, y))
         draw.line(line, fill=128)
 
+def read_labels(filename):
+    """Reads the csv file containing the labels format,
+    parses it to be accepted by `build_dataset`."""
+    outer = []
+    with open(filename) as csv:
+        for line in csv:
+            inner = []
+            for elem in line.split(','):
+                inner.append(int(elem.strip()))
+            outer.append(inner)
+    mat = numpy.array(outer).T
+    res = []
+    for line in mat.tolist():
+        res += line
+    return res
+
 def main():
     """main"""
     pic = Image.open('data/image0.jpg')
+    label0 = [0, 0, 0, 0, 0, 0, 0, 0] + \
+        [0, 0, 0, 0, 0, 0, 0, 0] + \
+        [0, 0, 0, 0, 1, 1, 1, 0] + \
+        [0, 0, 0, 0, 1, 0, 0, 0] + \
+        [1, 1, 0, 1, 1, 0, 0, 0] + \
+        [1, 0, 0, 1, 1, 0, 0, 0] + \
+        [0, 0, 0, 1, 0, 0, 0, 0] + \
+        [0, 0, 0, 0, 0, 0, 0, 0]
+
+    csv0 = read_labels('data/labels0.csv')
+    assert label0 == csv0
     parts = lowres(cleansing.grid(pic, (80, 60)))
     training_data = cleansing.build_dataset(
         cleansing.compress(parts),
-        [0, 0, 0, 0, 0, 0, 0, 0] +
-        [0, 0, 0, 0, 0, 0, 0, 0] +
-        [0, 0, 0, 0, 1, 1, 1, 0] +
-        [0, 0, 0, 0, 1, 0, 0, 0] +
-        [1, 1, 0, 1, 1, 0, 0, 0] +
-        [1, 0, 0, 1, 1, 0, 0, 0] +
-        [0, 0, 0, 1, 0, 0, 0, 0] +
-        [0, 0, 0, 0, 0, 0, 0, 0]
+        csv0
     )
     net = Network([20*15, 20*15, 20, 15, 1])
     net.SGD(training_data, 100, 8, 1, training_data)
